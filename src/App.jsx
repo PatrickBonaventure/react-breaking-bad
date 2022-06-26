58fd5301dd708538ca9aaaf6481460f8e5001fc5
PatrickBonaventure <patrickbonaventure92@gmail.com>
2022-06-26 16:34:06 +0200

    Page error add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {Routes , Route} from "react-router-dom"
 import MainPage from './components/MainPage/MainPage'
-import Error from "./components/error";
+import Error from "./components/Error";
 function App() {
   
 
@@ -11,8 +11,8 @@ function App() {
       <hr />
       
       <Routes>
-        <Route path="/react-breaking-bad" element={<MainPage/>} />
-        <Route path="/react-breaking-bad/error" element={<Error/>} />
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/error" element={<Error/>} />
       </Routes>
     </div>
   )
