f6b0cd81d1521956951aa901cd30a3429f21a18d
PatrickBonaventure <patrickbonaventure92@gmail.com>
2022-06-26 15:18:46 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {Routes , Route} from "react-router-dom"
 import MainPage from './components/MainPage/MainPage'
-import Error from "./components/Error";
+import Error from "./components/error";
 function App() {
   
 
@@ -10,8 +10,8 @@ function App() {
       <h1 className="text-center">Breaking Bad Wiki</h1>
       <hr />
       <Routes>
-        <Route path="/react-breaking-bad" element={<MainPage/>} />c
-        <Route path="/react-breaking-bad/error" element={<Error/>} />
+        <Route path="/" element={<MainPage/>} />
+        <Route path="/error" element={<Error/>} />
       </Routes>
     </div>
   )
